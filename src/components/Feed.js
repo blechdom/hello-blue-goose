93b45c9b157e2d6d5c090f70939c2e457610ca17
Kristin Galvin <blechdom@gmail.com>
2020-05-17 21:38:11 -0700

    add axios get

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
@@ -6,6 +6,17 @@ import Typography from "@material-ui/core/Typography";
 const Feed = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    async function getUsers() {
+      const result = await axios.get(
+        "https://jy7gj0jnx3.execute-api.us-east-2.amazonaws.com/default/hbgFunction"
+      );
+      setData(result);
+    }
+    getUsers();
+  }, []);
 
   const handleNameChange = (event) => {
     setName(event.target.value);
@@ -49,6 +60,9 @@ const Feed = () => {
 
           <button type="submit">Send</button>
         </form>
+      </Box>
+      <Box display="flex" justifyContent="center" m={4} p={4}>
+        {JSON.stringify(data)}
       </Box>
     </div>
   );
