24ca2181a5818324e9278755d3f1584fbcacf1c4
Kristin Galvin <blechdom@gmail.com>
2020-05-23 08:56:18 -0700

    graphql update

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,39 +1,6 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateTodo = /* GraphQL */ `
-  subscription OnCreateTodo {
-    onCreateTodo {
-      id
-      name
-      description
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const onUpdateTodo = /* GraphQL */ `
-  subscription OnUpdateTodo {
-    onUpdateTodo {
-      id
-      name
-      description
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const onDeleteTodo = /* GraphQL */ `
-  subscription OnDeleteTodo {
-    onDeleteTodo {
-      id
-      name
-      description
-      createdAt
-      updatedAt
-    }
-  }
-`;
 export const onCreateInstruction = /* GraphQL */ `
   subscription OnCreateInstruction {
     onCreateInstruction {
